23b65a7ad79e745bb54760bd9f31e94d890b2beb
Bút <Cloly031128@gmail.com>
2024-05-27 11:00:18 +0700

    update

--- src/main/java/com/example/spring_boot/ProductController.java
+++ src/main/java/com/example/spring_boot/ProductController.java
@@ -3,22 +3,65 @@ package com.example.spring_boot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;  // Chú ý đảm bảo import đúng thư viện
 import java.util.List;
 
 @Controller
 public class ProductController {
-    //Call model step 2
     @Autowired
     private ProductRepository productRepository;
 
-    //Step 1
     @GetMapping("/")
     public String productList(Model model) {
         List<Product> products = productRepository.findAll();
-        //Step 3 controller tra cho view
         model.addAttribute("products", products);
         return "product-list";
+    }
+
+    @GetMapping("/add")
+    public String showAddProductForm(Model model) {
+        model.addAttribute("product", new Product());
+        return "add-product";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(@Valid @ModelAttribute("product") Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-product";
+        }
+        productRepository.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+        model.addAttribute("product", product);
+        return "update-product";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") long id, @Valid @ModelAttribute("product") Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            product.setId(id);
+            return "update-product";
+        }
+        productRepository.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") long id, Model model) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+        productRepository.delete(product);
+        return "redirect:/";
     }
 }
